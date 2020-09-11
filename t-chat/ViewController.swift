48602322be0f29dff72de032ebcc0bc9dbfca931
Artur Gnedoy <archie682@hotmail.com>
2020-09-11 12:27:07 +0300

    Initial Commit

---
+++ t-chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  t-chat
+//
+//  Created by Артур Гнедой on 11.09.2020.
+//  Copyright © 2020 Артур Гнедой. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
